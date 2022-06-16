5d6638e412688d155e4a2ecc589a58c67972814c
Stephan Kämper <the.tester@seasidetesting.com>
2022-06-16 13:22:04 +0200

    Adds the script
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>

--- check_bundler_versions.rb
+++ check_bundler_versions.rb
@@ -2,23 +2,24 @@
 # frozen_string_literal: true
 
 if ARGV.size != 1
-  puts "Need a folder name with Ruby projects as the (only) argument"
+  puts 'Need a folder name with Ruby projects as the (only) argument'
   exit 1
 end
 
-dev_folder = ARGV.first
+dev_folder     = ARGV.first
 search_pattern = File.join(dev_folder, '**', 'Gemfile.lock')
 
 fns = Dir[search_pattern]
 if fns.empty?
-  puts "No Gemfile.lock files found"
+  puts 'No Gemfile.lock files found'
 else
 
   fns.each do |fn|
-    puts "#{fn}: -> Bundler version: #{begin
-      File.read(fn).scan(/BUNDLED WITH\n\s*(.+)/).last.last
-    rescue StandardError
-      'NONE???'
-    end}"
+    puts "#{fn}: -> Bundler version: #{
+      begin
+        File.read(fn).scan(/BUNDLED WITH\n\s*(.+)/).last.last
+      rescue StandardError
+        'NONE???'
+      end}"
   end
 end
